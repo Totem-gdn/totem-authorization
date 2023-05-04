1af7d02f90bb5b891f64e9262581b9e9f997000f
timoshchuk-web <irunarpg0@gmail.com>
2023-05-05 01:15:38 +0300

    sockets upd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { SocketIoService } from "./core/services/socket-io.service";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  constructor(private socketIoService: SocketIoService) {}
+  ngOnInit(): void {
+    this.socketIoService.init();
+    this.socketIoService.listen();
+  }
 }
